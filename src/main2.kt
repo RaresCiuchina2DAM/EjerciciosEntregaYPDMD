e7e1a0c639ada70e7353428df80c2ed429e3c2a9
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2022-10-04 12:46:32 +0200

    Clases Ej 2 04102022 Hecho y funcionando

--- src/main2.kt
+++ src/main2.kt
@@ -6,8 +6,55 @@ fun main() {
 
     crearRascacielos(numeroPisos,numeroVentanas,numeroTorres)
     println("-----------------------------------------------------\n Ejercicio Listas de varios tipos:\n")
-
     listas_varios_tipos()
+
+
+
+    println("-----------------------------------------------------\n")
+    //EJERCICIO CLASES 2
+    var dado1 = Dado();
+    var dado2 = Dado(1,3)
+    var dado3 = Dado(4,6)
+    var jugadores = Array<Int>(10) {0}
+
+
+    repeat(jugadores.size) {
+
+        for (i in 0..jugadores.size-1) {
+            jugadores[i]= jugadores[i] +
+                    dado1.tiradaUnica() +
+                    dado2.tiradaUnica() +
+                    dado3.tiradaUnica()
+            println(jugadores[i])
+        }
+
+    }
+            darResultado(jugadores)
+
+    println("-----------------------------------------------------\n")
+
+
+}
+
+fun darResultado(jugadores : Array<Int>) {
+
+    ordenarMayor(jugadores,jugadores.size)
+    for (i in 0..jugadores.size-1){
+        println("Jugador : " +  i + " tiene: " + jugadores[i] + " puntos" )
+    }
+}
+
+fun ordenarMayor(listNum: Array<Int>, cant: Int) {
+    var tmp = 0
+    for(x in 0 until cant) {
+        for(y in 0 until cant) {
+            if(listNum[x] > listNum[y]) {
+                tmp = listNum[x]
+                listNum[x] = listNum[y]
+                listNum[y] = tmp
+            }
+        }
+    }
 
 }
 
@@ -22,8 +69,6 @@ fun crearRascacielos(numeroPisos: Int, numeroVentanas:Int, numeroTorres:Int) {
         }
     }
     println()
-
-
         repeat(numeroPisos) {
             repeat(numeroTorres) {
                 print("|")
@@ -39,11 +84,10 @@ fun crearRascacielos(numeroPisos: Int, numeroVentanas:Int, numeroTorres:Int) {
         }
     }
 
-
 fun listas_varios_tipos(){
     val lista = listOf("Hola,", 2, 3, "¿", 0.1, "Qué", 1, "tal", 0.9, "?")
     var contStrings =""
-    var sumDouble = ""
+    var sumDouble = 0.0
 
 
     lista.forEach {
@@ -58,6 +102,9 @@ fun listas_varios_tipos(){
     println("El contenido sumatorio es : $sumDouble")
 
 }
+
+
+
 
 
 
