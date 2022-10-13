ba2445dc3e33ef8030a405d558352202891bbdec
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2022-10-13 14:11:26 +0200

    Ej 5 sin hace Ej 6 Hecho Ej 7 empezado

--- src/MochilaMain.kt
+++ src/MochilaMain.kt
@@ -1,18 +1,31 @@
+import kotlin.math.max
+
 fun main() {
     var Ladron = Persona("Ladrón","Jack",100)
-    var objeto1 = Objeto(0,0)
-    var objeto2 = Objeto(,95, 10 )
+    var objeto1 = Objeto(10,50)
+    var objeto2 = Objeto(95, 10 )
     var objeto3 = Objeto(5,155)
-    var articulos = arrayOf(objeto1,objeto2,objeto3)
+    val listaObjetos = arrayOf<Objeto>(objeto1,objeto2,objeto3)
+
+
+    cogerConMasValor(Ladron,listaObjetos)
+
 
 }
 
 fun cogerConMasValor(Ladron : Persona , articulos : Array<Objeto> ){
-    var puedeEntrar = arrayOf<Objeto>()
-    for (i in articulos.indices){
-        if (articulos[i].peso < Ladron.capacidadPesoMochilaMax ){
-            puedeEntrar[i] = articulos[i]
-
-        }
+    val rentabilidad = Array<Double>(size = articulos.size){0.00}
+    for (i  in articulos.indices){
+        rentabilidad[i] = (articulos[i].valor/articulos[i].peso).toDouble()
+        println(rentabilidad[i])
     }
+    println(rentabilidad.max())
+
+    for (i in rentabilidad.indices){
+        if (rentabilidad.max() =)
+        if ()
+
+    }
+
+
 }
