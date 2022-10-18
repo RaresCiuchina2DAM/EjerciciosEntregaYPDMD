b2524f32d4171fd4b11790a05fddc28af5c77ab8
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2022-10-18 14:19:14 +0200

    Ej 7 empezado y con algo de idea1 Ej 5 sin hace Ej 6 Hecho
    Modificado y organizado por ejercicios
    EJEERCICIO HECHOSSS

--- src/MochilaMain.kt
+++ src/MochilaMain.kt
@@ -1,44 +1,47 @@
-import kotlin.math.max
-
 fun main() {
-    var Ladron = Personaje("Ladrón","Jack",10)
-    var objeto1 = Objeto(5,10)
-    var objeto2 = Objeto(4, 40 )
+    var Ladron = Jugador("Ladrón", "Jack", 10, 25, "Elfo","")
+    var objeto1 = Objeto(7,1000)
+    var objeto2 = Objeto(1, 100 )
     var objeto3 = Objeto(6,30)
     var objeto4 = Objeto(4,50)
     val listaObjetos = arrayOf<Objeto>(objeto1,objeto2,objeto3,objeto4)
 
 
-    cogerConMasValor(Ladron,listaObjetos)
-
+    Robar(Ladron,listaObjetos)
+    hablarConJugador("¿COMO ESTAS?",Ladron)
 
 }
 
-fun cogerConMasValor(Ladron : Personaje , articulos : Array<Objeto> ){
+fun Robar(Ladron : Jugador , articulos : Array<Objeto> ){
+    //Saca la rentabilidad de cada objeto y lo añade en orden a un array del mismo tamaño que el original
     val rentabilidad = Array<Double>(size = articulos.size){0.00}
     for (i  in articulos.indices) {
         rentabilidad[i] = (articulos[i].valor / articulos[i].peso).toDouble()
-        println(rentabilidad[i])
-
         }
-    println("----------------------")
-    println(rentabilidad.max())
 
-        var acum=0.0
+    //Recorre el array una y otra vez, hasta que no entre nada más en la mochila
+    while (Ladron.capacidadPesoMochilaMax > Ladron.sumPeso && rentabilidad.max() != 0.0){
         for (i in rentabilidad.indices) {
-            if (rentabilidad[i] == rentabilidad.max() && Ladron.capacidadPesoMochilaMax >= articulos[i].peso) {
-                acum += articulos[i].peso.toDouble()
-                Ladron.sumValor = articulos[i].valor
+            if ( rentabilidad[i] == rentabilidad.max() ) {
+                if (Ladron.sumPeso + articulos[i].peso > Ladron.capacidadPesoMochilaMax ){
+                    Ladron.sumPeso //no sufre modificaciones
+                    Ladron.sumValor //no sufre modificaciones
+                }
+                if (Ladron.sumPeso + articulos[i].peso <= Ladron.capacidadPesoMochilaMax) {
+                    Ladron.sumPeso += articulos[i].peso
+                    Ladron.sumValor += articulos[i].valor
+                }
+
                 rentabilidad[i] = 0.0
                 articulos[i].valor = 0
                 articulos[i].peso = 0
             }
 
         }
-        Ladron.capacidadPesoMochilaMax = acum
+    }
+    //imprime el contenido
+    println(Ladron.toString())
 
-
-    println(" " + Ladron.capacidadPesoMochilaMax +  " " + Ladron.sumValor)
 
 
 }
